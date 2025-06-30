3f05ae98ec8ffb31a288f07a02049b3a881a4e2b
dasehigg <dasean-labs@gmail.com>
2025-06-29 20:01:08 -0700

    Update Terraform files for Phase 2 Azure segmentation lab

--- phase-2-azure-segmentation/terraform-azure-secure-vnet/main.tf
+++ phase-2-azure-segmentation/terraform-azure-secure-vnet/main.tf
@@ -1,5 +1,8 @@
 provider "azurerm" {
     features {}
+
+    subscription_id = "34ed375f-f1bc-4ee9-a6c9-5dd357ed60a0"
+    tenant_id = "62279278-6b42-442e-8e29-979f0b134ae1"
 }
 
 resource "azurerm_resource_group" "main" {
@@ -9,7 +12,7 @@ resource "azurerm_resource_group" "main" {
 
 resource "azurerm_virtual_network" "main" {
     name = "vnet-securelab"
-    address_space = ["10.0.0.0/16"0]
+    address_space = ["10.0.0.0/16"]
     location = var.location
     resource_group_name = azurerm_resource_group.main.name
 }
@@ -31,10 +34,10 @@ resource "azurerm_network_security_group" "main" {
         priority = 100
         direction = "Inbound"
         access = "Allow"
-        protocol = 'Tcp"
+        protocol = "Tcp"
         source_port_range = "*"
         destination_port_range = "3389"
         source_address_prefix = "*"
-        destinnation_address_prefix = "*"
+        destination_address_prefix = "*"
     }
 }
